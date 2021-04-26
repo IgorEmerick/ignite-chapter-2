2a18426e0675693e6f0633ba085bec2c139f2155
Igor Emerick <igorbarbosaemerick@gmail.com>
2021-04-26 07:56:15 -0300

    Refatorando ListAvailableCarsController

--- src/modules/cars/controllers/cars/ListAvailableCarsController.ts
+++ src/modules/cars/controllers/cars/ListAvailableCarsController.ts
@@ -3,14 +3,14 @@ import { container } from "tsyringe";
 import ListAvailableCarsService from "../../services/cars/ListAvailableCarsService";
 
 export default class ListAvailableCarsController {
-  public async handle(req: Request, res: Response) {
+  public async handle(req: Request, res: Response): Promise<Response> {
     const { brand, category_id, name, } = req.query;
     const listAvailableCarsService = container.resolve(ListAvailableCarsService);
 
     const cars = await listAvailableCarsService.execute({
-      brand: (brand ? brand.toString() : undefined),
-      category_id: (category_id ? category_id.toString() : undefined),
-      name: (name ? name.toString() : undefined),
+      brand: brand as string,
+      category_id: category_id as string,
+      name: name as string,
     });
 
     return res.json(cars);
